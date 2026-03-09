322cf9d6c7476f39f8cd217624d158e16a0c2c10
fops <tcpcodeine@gmail.com>
2026-03-09 10:16:47 +0100

    add

---
+++ main/main.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+#inlcude "/interfacing/interfacing.h"
+#include "../wine/loader/main.h"
+
+int main() {
+//initialize interfacing between wine and vodka, then run debugger and shit
+}
